c792dd0e66ec07bdef2f5470a8461e7f1543e98c
Guillem <guillem@disedit.com>
2025-04-26 10:29:50 +0200

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,21 @@
+import tailwindcss from "@tailwindcss/vite"
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: false },
+
+  css: ['~/assets/css/main.css'],
+
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/icon',
+    '@nuxt/image'
+  ],
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+})
